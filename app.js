2b2d15c0ed7d47873f53b4683cd78828c6e17766
lks5k <lukas_enrique5k@hotmail.com>
2026-04-19 17:13:58 -0500

    Carrito con localStorage y botón de Vaciado

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+
 const contenedor = document.getElementById("contenedor-productos")
 
-productos.forEach(function(producto) {
+productos.forEach(function (producto) {
     contenedor.innerHTML += `
     <div>
         <h3>${producto.nombre}</h3>
         <p>$ ${producto.precio.toLocaleString("es-CO")}</p>
+        <button onclick="agregarAlCarrito('${producto.nombre}', ${producto.precio})">
+        Agregar al Carrito
+        </button>
     </div>
-    `    
+    `
 })
+
+function agregarAlCarrito(nombre, precio) {
+    carrito.push({ nombre: nombre, precio: precio })
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    mostrarCarrito()
+}
+
+function mostrarCarrito() {
+    const contenedorCarrito = document.getElementById("contenedor-carrito")
+    contenedorCarrito.innerHTML = ""
+    carrito.forEach(function (item) {
+        contenedorCarrito.innerHTML += `
+        <p>${item.nombre} - $${item.precio.toLocaleString("es-CO")}</P>
+        `
+    })
+}
+
+function vaciarCarrito() {
+    carrito = []
+    localStorage.clear()
+    mostrarCarrito()
+}
+
+mostrarCarrito()
--- productos.js
+++ productos.js
@@ -1,17 +1,27 @@
 const productos = [
     {
-    nombre: "Camiseta",
-    precio: 35000,
-    imagen: "camisa.jpg"
+        nombre: "Camiseta",
+        precio: 35000,
+        imagen: "camisa.jpg"
     },
     {
-    nombre: "Blusa",
-    precio: 40000,
-    imagen: "blusa.jpg"
+        nombre: "Blusa",
+        precio: 40000,
+        imagen: "blusa.jpg"
     },
     {
-    nombre: "Pantalón",
-    precio: 65000,
-    imagen: "pantalon.jpg"
+        nombre: "Pantalón",
+        precio: 65000,
+        imagen: "pantalon.jpg"
+    },
+    {
+        nombre: "Maletín",
+        precio: 47000,
+        imagen: "maletin.jpg"
+    },
+    {
+        nombre: "Corbata",
+        precio: 19000,
+        imagen: "corbata.jpg"
     }
 ]
